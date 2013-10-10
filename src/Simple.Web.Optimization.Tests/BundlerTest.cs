a8fed708851815048f52834e5c4d861bcd777477
Tomas Jansson <tomas.jansson@gmail.com>
2013-10-10 21:10:54 +0200

    Added Simple.Web.Optimization

---
+++ src/Simple.Web.Optimization.Tests/BundlerTest.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using Xunit;
+
+namespace Simple.Optimization.Tests
+{
+    public class BundlerTests
+    {
+        [Fact]
+        public void AddScriptBundlerShouldReturnBundlerDefinition()
+        {
+            var bundle = new ScriptBundle("assets/js");
+            var actual = Bundler.Add(bundle);
+
+            Assert.IsType<BundleDefinition>(actual);
+            Assert.NotNull(actual);
+        }
+
+        [Fact]
+        public void AddScriptBundleWithSameNameTwiceShouldSameDefinition()
+        {
+            var bundle = new ScriptBundle("assets/js");
+            var bundle2 = new ScriptBundle("assets/js");
+            var expected = Bundler.Add(bundle);
+            var actual = Bundler.Add(bundle2);
+
+            Assert.Same(expected, actual);
+        }
+    }
+
+    public class BundlerDefinitionTests
+    {
+        private string _bundlePath = "assets/js";
+        private IBundle _bundle;
+
+        [Fact]
+        public void AddOneReturnsSameDefinitionWithOneFile()
+        {
+            var definition = CreateDefinition();
+            var actual = definition.Include("assets/js/file1.js");
+            Assert.Same(definition, actual);
+
+            var files = Bundler.GetFiles(_bundlePath).ToList();
+            Assert.Equal(1, files.Count);
+            Assert.Equal("file1.js", files[0].Name);
+        }
+
+        [Fact]
+        public void AddingNonExistingFileThrowsFileNotFound()
+        {
+            var definition = CreateDefinition();
+            Assert.Throws<FileNotFoundException>(() => definition.Include("assets/js/fileNotFound.js"));
+        }
+
+        [Fact]
+        public void AddingTwoExistingFilesShouldBeInTheRightOrderWhenGettingThem()
+        {
+            var definition = CreateDefinition();
+            var actual = definition.Include("assets/js/file1.js", "assets/js/file2.js");
+            Assert.Same(definition, actual);
+
+            var files = Bundler.GetFiles(_bundlePath).ToList();
+            Assert.Equal(2, files.Count);
+            Assert.Equal("file1.js", files[0].Name);
+            Assert.Equal("file2.js", files[1].Name);
+        }
+
+        private BundleDefinition CreateDefinition()
+        {
+            Bundler.Reset();
+            _bundle = new ScriptBundle(_bundlePath);
+            return Bundler.Add(_bundle);
+        }
+    }
+
+    public class BundlerRendererTests
+    {
+        private IBundle _bundle;
+        private BundleDefinition _definition;
+        private string _bundlePath = "assets/js";
+
+
+        public BundlerRendererTests()
+        {
+            _bundle = new ScriptBundle("assets/js");
+            _definition = Bundler.Add(_bundle);
+            BundlerSettings.Optimize = false;
+        }
+
+        [Fact]
+        public void RenderOneScriptWithNoOptimizationAndOneFileGiveTag()
+        {
+            BundlerRenderer.Render(_bundlePath);
+        }
+    }
+}
---
+++ src/Simple.Web.Optimization/Bundler.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace Simple.Optimization
+{
+    public static class Bundler
+    {
+        private static BundleCollection _bundleCollection = new BundleCollection();
+        public static BundleDefinition Add(IBundle bundle)
+        {
+            return _bundleCollection.Add(bundle);
+        }
+
+        public static IEnumerable<FileInfo> GetFiles(string bundlePath)
+        {
+            return _bundleCollection.GetFiles(bundlePath);
+        }
+
+        public static void Reset()
+        {
+            _bundleCollection = new BundleCollection();
+        }
+    }
+
+    internal static class PathHelper
+    {
+        private static string AppRoot = AssemblyAppRoot(typeof (PathHelper).Assembly.GetPath());
+
+        public static string GetPath(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                return filePath;
+            }
+
+            filePath = Path.Combine(AppRoot, filePath);
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException();
+            }
+            return filePath;
+        }
+        
+        private static string AssemblyAppRoot(string typePath)
+        {
+            var pattern = @"(\\|/)bin(\\|/)?([Dd]ebug|[Rr]elease)?$";
+            var target = Path.GetDirectoryName(typePath);
+            return new Regex(pattern).Replace(target, string.Empty);
+        }
+
+        private static string GetPath(this Assembly assembly)
+        {
+            return new Uri(assembly.EscapedCodeBase).LocalPath;
+        }
+    }
+
+    internal class BundleCollection
+    {
+        private Dictionary<IBundle, BundleDefinition> _bundleDefinitions = new Dictionary<IBundle, BundleDefinition>();
+
+        internal BundleDefinition Add(IBundle bundle)
+        {
+            if (!_bundleDefinitions.ContainsKey(bundle))
+            {
+                _bundleDefinitions.Add(bundle, new BundleDefinition());
+            }
+            return _bundleDefinitions[bundle];
+        }
+
+        public IEnumerable<FileInfo> GetFiles(string bundlePath)
+        {
+            var definition = _bundleDefinitions.Single(y => y.Key.BundlePath == bundlePath).Value;
+            return definition.GetFiles();
+        }
+    }
+
+    public class ScriptBundle : Bundle
+    {
+        public ScriptBundle(string bundleName) : base(bundleName)
+        {
+        }
+    }
+
+    public class BundleDefinition
+    {
+        private Collection<FileInfo> _fileInfos = new Collection<FileInfo>();
+
+        public BundleDefinition Include(params string[] paths)
+        {
+            foreach (var path in paths)
+            {
+                var qualifiedPath = PathHelper.GetPath(path);
+                _fileInfos.Add(new FileInfo(qualifiedPath));                
+            }
+            return this;
+        }
+
+        public IEnumerable<FileInfo> GetFiles()
+        {
+            return _fileInfos;
+        }
+    }
+
+    public interface IBundle
+    {
+        string BundlePath { get; }
+    }
+
+    public abstract class Bundle : IBundle
+    {
+        private readonly string _bundleName;
+
+        protected Bundle(string bundleName)
+        {
+            _bundleName = bundleName;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            return Equals(obj as Bundle);
+        }
+
+        protected bool Equals(Bundle other)
+        {
+            return string.Equals(_bundleName, other._bundleName);
+        }
+
+        public override int GetHashCode()
+        {
+            return (_bundleName != null ? _bundleName.GetHashCode() : 0);
+        }
+
+        public string BundlePath { get { return _bundleName; } }
+    }
+
+    public static class BundlerSettings
+    {
+        private static bool _optimize = true;
+
+        public static bool Optimize
+        {
+            get
+            {
+                return _optimize;
+            }
+            set
+            {
+                _optimize = value;
+            }
+        }
+    }
+
+    public static class BundlerRenderer
+    {
+        public static void Render(string bundlePath)
+        {
+            var files = Bundler.GetFiles(bundlePath);
+        }
+    }
+}
